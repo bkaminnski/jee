2cbea579e9b2a95f44e2f9bc65d00bc5fa0bfc85
Bartosz Kamiński <bartosz.kaminski@powel.no>
2017-08-27 09:28:20 +0200

    for ThreadHolder, package-private scope is enough

--- 01-misleading-asynchronous-future/src/main/java/com/hclc/jee/misleading/asynchronous/stoppable/asynchronous/interruptingthread/control/ThreadHolder.java
+++ 01-misleading-asynchronous-future/src/main/java/com/hclc/jee/misleading/asynchronous/stoppable/asynchronous/interruptingthread/control/ThreadHolder.java
@@ -1,13 +1,13 @@
 package com.hclc.jee.misleading.asynchronous.stoppable.asynchronous.interruptingthread.control;
 
-public class ThreadHolder {
+class ThreadHolder {
     private volatile Thread thread;
 
-    public void keep(Thread thread) {
+    void keep(Thread thread) {
         this.thread = thread;
     }
 
-    public void interrupt() {
+    void interrupt() {
         thread.interrupt();
     }
 }
